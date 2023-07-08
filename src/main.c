7a5d868f73d3a14a268bcec1ee1bf218b1565f42
Anthony Volk <anth.volk@gmail.com>
2023-07-08 00:28:50 -0600

    chore: outline project in main()

--- src/main.c
+++ src/main.c
@@ -6,7 +6,36 @@
 // Main function
 int main(void) {
 
-	
+	// Program will take two args: an infile and an outfile
+
+	// Throw error if user doesn't input both infile and outfile
+
+	// Throw error if infile isn't of type 'csv'
+
+	// Throw error if outfile isn't of type 'json'
+
+	// Attempt to open infile
+
+	// If unable to open infile, crash
+
+	// Attempt to open outfile
+
+	// If unable to open outfile, crash
+
+	// Set up file reader with delimiter
+
+	// Read first line into memory and store as an array
+
+	// For each next successive line, until EOF...
+		// Print key:value, using relevant tag from array as key, so long as key and value exist, else skip
+
+	// Close file
+
+	// Free any heap-allocated memory
+
+	// Print success message
+
+	// Exit
 
 
 }
