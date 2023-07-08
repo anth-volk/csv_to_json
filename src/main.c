adec915548d7246e2ca8f3a06e5ce9c6570f0f55
Anthony Volk <anth.volk@gmail.com>
2023-07-08 01:40:16 -0600

    feat: Infile error testing

--- src/main.c
+++ src/main.c
@@ -1,16 +1,66 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
+// Number of arguments to be input by user
+const int NUM_OF_ARGS = 2;
 
+// Default infile format data
+const int INFILE_EXT_LEN = 4;
+const char INFILE_FORMAT[INFILE_EXT_LEN] = ".csv";
+
+// Test to make sure argc is correct number
+bool errorTestArgc(argc) {
+	if (argc != NUM_OF_ARGS) {
+		return false;
+	}
+
+	return true;
+}
+
+// Test to make sure infile address is formatted correctly
+bool errorTestInfile(arg) {
+
+	// If arg doesn't exist, return false
+	if (!arg) {
+		return false;
+	}
+
+	// Obtain arg's length
+	const int argLength = strlen(arg);
+
+	// If arg is too short, return false
+	if (argLength < INFILE_EXT_LEN) {
+		return false;
+	}
+
+	// If the end of the arg is not the extension,
+	// return false
+	if (!strcasecmp(strrchr(arg, '\0' - INFILE_EXT_LEN), INFILE_FORMAT)) {
+		return false;
+	}
+
+	return true;
+
+}
 
 
 // Main function
-int main(void) {
+int main(int argc, char* argv[]) {
 
 	// Program will take two args: an infile and an outfile
 
 	// Throw error if user doesn't input both infile and outfile
+	if (!errorTestArgc(argc)) {
+		printf("Program requires two arguments: a CSV-formatted infile and a JSON-formatted destination\n");
+		return 1;
+	}
 
 	// Throw error if infile isn't of type 'csv'
+	if (!errorTestInfile(argv[0])) {
+		printf("Program requires a CSV-formatted infile as the first argument, including the .csv extension\n");
+		return 2;
+	}
 
 	// Throw error if outfile isn't of type 'json'
 
