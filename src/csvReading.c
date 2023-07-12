597e4ba5b631866f93282c131c2cfad4342d8585
Anthony Volk <anth.volk@gmail.com>
2023-07-12 16:52:56 -0600

    chore: csvReading function architecting

--- src/csvReading.c
+++ src/csvReading.c
@@ -1,3 +1,16 @@
 
 #include "../headers/header.h"
 
+// Function to read a line
+
+// Function to tokenize a line, 
+// then return an array
+
+// Function that takes key and value array, 
+// formats both, and returns a string that 
+// represents one object inside the JSON structure
+
+// Function to print starting JSON sequence
+
+// Function to print ending JSON sequence
+
