27830df853e5264ad2b8083a78c7da0c9890b79d
Anthony Volk <anth.volk@gmail.com>
2023-07-15 16:42:23 -0600

    feat: Error-fixing in tests

--- tests/node.c
+++ tests/node.c
@@ -9,16 +9,16 @@ int main(void) {
 	// Test creation of an empty node
 	node* newNode = node_createEmptyNode();
 	assert(newNode->next == NULL);
-	printf("node_createEmptyNode() creates new node with empty 'next' value");
+	fprintf(stdout, "node_createEmptyNode() creates new node with empty 'next' value\n");
 	assert(newNode->word == NULL);
-	printf("node_createEmptyNode() creates new node with empty 'word' value");
+	fprintf(stdout, "node_createEmptyNode() creates new node with empty 'word' value\n");
 
 	// Test creation of a filled node
 	node* newNode2 = node_createFilledNode(TEST_INPUT);
-	assert(newNode->next == NULL);
-	printf("node_createFilledNode() creates new node with empty 'next' value");
-	assert(newNode->word == TEST_INPUT);
-	printf("node_createFilledNode() creates new node with filled 'word' value");
+	assert(newNode2->next == NULL);
+	fprintf(stdout, "node_createFilledNode() creates new node with empty 'next' value\n");
+	assert(newNode2->word == TEST_INPUT);
+	fprintf(stdout, "node_createFilledNode() creates new node with filled 'word' value\n");
 
 	// Test addLast()
 
@@ -29,11 +29,11 @@ int main(void) {
 	// Test
 	node* addedNode = node_addLast(&head, &tail, TEST_INPUT);
 	assert(head == addedNode);
-	printf("node_addLast() creates new node and assigns head pointer properly");
+	fprintf(stdout, "node_addLast() creates new node and assigns head pointer properly\n");
 	assert(tail == addedNode);
-	printf("node_addLast() assigns tail pointer to added node correctly");
+	fprintf(stdout, "node_addLast() assigns tail pointer to added node correctly\n");
 	assert(head->word == TEST_INPUT);
-	printf("node_addLast() assigns new 'word' value properly");
+	fprintf(stdout, "node_addLast() assigns new 'word' value properly\n");
 
 	return 0;
 
